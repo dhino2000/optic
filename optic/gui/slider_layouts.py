fa1977b30e5dcf70b9b941df2731f50249246c52
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2024-08-23 21:20:52 +0900

    upper middle layout, slider, channel checkbox

--- optic/gui/slider_layouts.py
+++ optic/gui/slider_layouts.py
@@ -1,42 +1,41 @@
 # sliderがメインのlayout作成用関数
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
+from .base_layouts import makeLayoutSliderLabel
 
 # 画像のコントラスト調節スライダー、表示チェックボックス用Layout
-def makeLayoutContrastSlider(self, key, label_checkbox, label_label, func_slider=None, func_checkbox=None):
+def makeLayoutContrastSlider(widget_manager, key_label, key_checkbox, key_slider, label_checkbox, label_label, func_slider=None, func_checkbox=None, checked=True):
     layout = QVBoxLayout()
     # チェックボックスの設定
-    layout.addWidget(self.makeWidgetCheckBox(key, label_checkbox, func_=func_checkbox, checked=True))
+    layout.addWidget(widget_manager.makeWidgetCheckBox(key=key_checkbox, label=label_checkbox, func_=func_checkbox, checked=checked))
 
     # Min, Max Valueスライダーの設定
     for m, value_set in zip(["min", "max"], [0, 255]):
-        layout.addLayout(self.makeLayoutSliderLabel(key_label=f"value_{key}_{m}", 
-                                                    key_slider=f"value_{key}_{m}", 
-                                                    label=f"{m} {label_label}", 
-                                                    value_set=value_set, 
-                                                    func_=func_slider))
+        layout.addLayout(makeLayoutSliderLabel
+                            (
+                            widget_manager,
+                            key_label=f"{key_label}_{m}", 
+                            key_slider=key_slider, 
+                            label=f"{m} {label_label}", 
+                            value_set=value_set, 
+                            func_=func_slider
+                            )
+                        )
     return layout
 
-# 透明度調節スライダーのLayout
-def makeLayoutOpacitySlider(self):
+# 透明度調節スライダーのLayout, ALL ROI, Selected ROI
+def makeLayoutOpacitySlider(widget_manager, key_label, key_slider, label, func_slider=None):
     layout = QHBoxLayout()
-    for (key, label, default_value) in zip(['opacity_allroi', 'opacity_selectedroi'],
-                                            ['Opacity of All ROI', 'Opacity of Selected ROI'],
-                                            [50, 255]):
-        layout.addLayout(self.makeLayoutSliderLabel(key_label=key, 
-                                                    key_slider=key, 
-                                                    label=label, 
-                                                    value_set=default_value,
-                                                    func_=lambda value, k="pri": self.onSliderValueChanged(k)))
-    return layout
-
-# スライダーをまとめたLayout
-def makeLayoutAllSlider(self):
-    layout = QGridLayout()
-    for i, channel in enumerate(["pri", "sec"]):
-        layout.addLayout(self.makeLayoutContrastSlider(key=f"{channel}", 
-                                                        label_checkbox=f"Show {channel}", 
-                                                        label_label=f"Value ({channel})",
-                                                        func_checkbox=lambda state, key="pri", channel=channel: self.toggleChannelVisibility(key, state, channel),
-                                                        func_slider=lambda value, k="pri": self.onSliderValueChanged(k)), 
-                                                        1, i+1, 1, 1)
-    layout.addLayout(self.makeLayoutOpacitySlider(), 2, 1, 1, 2)
+    for (key_, label_, default_value) in zip(['opacity_allroi', 'opacity_selectedroi'],
+                                             ['Opacity of All ROI', 'Opacity of Selected ROI'],
+                                             [50, 255]):
+        layout.addLayout(makeLayoutSliderLabel
+                            (
+                            widget_manager,
+                            key_label=f"{key_label}_{key_}", 
+                            key_slider=f"{key_slider}_{key_}", 
+                            label=f"{label} {label_}", 
+                            value_set=default_value,
+                            func_=func_slider
+                            )
+                        )
     return layout
