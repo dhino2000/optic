4109ca35276494d21e378d518cc7ce8dbae72d03
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2024-08-26 15:21:13 +0900

    update Upper Right Layout, update io_layouts, makeLayoutROICheckIO

--- optic/gui/io_layouts.py
+++ optic/gui/io_layouts.py
@@ -1,7 +1,6 @@
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QRadioButton, QButtonGroup, QSlider
 from PyQt5.QtCore import Qt
 from .base_layouts import makeLayoutLineEditLabel
-from ..io.file_dialog import openFileDialogAndSetLineEdit
 
 # 読み込むファイルを選択するためのウィジェット
 def makeLayoutLoadFileWidget(widget_manager, label="", key_label="", key_lineedit="", key_button=""):
@@ -10,3 +9,11 @@ def makeLayoutLoadFileWidget(widget_manager, label="", key_label="", key_lineedi
     layout.addWidget(widget_manager.makeWidgetButton(key=key_button, label="Browse"))
     return layout
 
+# Table, ROICheck, ROIMatchのIO
+def makeLayoutROICheckIO(widget_manager, key_button):
+    layout = QHBoxLayout()
+    list_key = [f"{key_button}_save_roicheck", f"{key_button}_load_roicheck"]
+    list_label = ["Save ROICheck", "Load ROICheck"]
+    for key, label in zip(list_key, list_label):
+        layout.addWidget(widget_manager.makeWidgetButton(key=key, label=label))
+    return layout
