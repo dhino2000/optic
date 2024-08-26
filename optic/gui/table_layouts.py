25c404c9705e32c035b0e42936755ad8f6ba45e1
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2024-08-26 15:11:32 +0900

    update Upper Right Layout, update table_layouts makeLayoutAllROISetSameCelltype

--- optic/gui/table_layouts.py
+++ optic/gui/table_layouts.py
@@ -15,3 +15,12 @@ def makeLayoutTableROICountLabel(widget_manager, key_label, key_table):
     layout.addWidget(q_table)
     layout.addLayout(layout_label)
     return layout
+
+# 全てのROIを同じcelltypeにそろえる
+def makeLayoutAllROISetSameCelltype(widget_manager, key_button):
+    layout = QHBoxLayout()
+
+    list_celltype = [key for key in widget_manager.dict_tablecol.keys() if widget_manager.dict_tablecol[key].get("group") == "celltype"]
+    for celltype in list_celltype:
+        layout.addWidget(widget_manager.makeWidgetButton(key=f"{key_button}_roi_set_{celltype}", label=f"Set {celltype}"))
+    return layout
