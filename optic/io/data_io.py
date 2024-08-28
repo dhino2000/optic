44b5fb2a53307ee7cfa209677cc23d82315d96e7
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2024-08-28 19:09:54 +0900

    update data_io, generateSavePath

--- optic/io/data_io.py
+++ optic/io/data_io.py
@@ -1,10 +1,13 @@
 # データの読み込み用関数
+import os
 from PyQt5.QtWidgets import QMessageBox
 from scipy.io import loadmat
 import tifffile
 import datetime
 from ..preprocessing.preprocessing_fall import convertMatToDictFall, convertMatToDictROICheck
 from ..preprocessing.preprocessing_image import convertImageDtypeToINT, resizeImageShape
+from ..preprocessing.preprocessing_table import convertTableDataToDictROICheck, convertDictROICheckToMatROICheck
+from .file_dialog import openFileDialog, saveFileDialog
 
 # Fallの読み込み, メッセージ付き
 def loadFallMAT(q_window, data_manager, key_dict_Fall, path_fall, preprocessing=True):
@@ -35,10 +38,42 @@ def clearEventFileNPY(widget_manager):
     pass
 
 
+# 保存用のファイルパス作成, 初期位置も指定
+def generateSavePath(path_src, prefix="", suffix="", new_extension=None, remove_strings=None):
+    """
+    元のファイルパスを基に、新しい保存パスを生成する。
 
+    :param path_src: 元のファイルパス
+    :param prefix: 新しいファイル名の接頭辞
+    :param suffix: 新しいファイル名の接尾辞
+    :param new_extension: 新しい拡張子（指定しない場合は元の拡張子を使用）
+    :param remove_strings: 元のファイル名から除去する文字列または文字列のリスト
+    :return: 生成された保存パス
+    """
+    dir_src = os.path.dirname(path_src)
+    name_ext_src =  os.path.basename(path_src)
+    name_src, ext_src = os.path.splitext(name_ext_src)
+
+    # 指定された文字列を除去
+    if remove_strings:
+        if isinstance(remove_strings, str):
+            remove_strings = [remove_strings]
+        for s in remove_strings:
+            name_src = name_src.replace(s, '')
+
+    name_dst = f"{prefix}{name_src}{suffix}"
+
+    # 新しい拡張子が指定されている場合は使用、そうでなければ元の拡張子を使用
+    ext_dst = new_extension if new_extension else ext_src
+
+    name_ext_dst = f"{name_dst}{ext_dst}"
+    path_dst = os.path.join(dir_src, name_ext_dst).replace("\\", "/")
+    return path_dst
 
 # Tableの内容をROICheckとして保存
-def saveROICheck(data_manager):
+def saveROICheck(q_window, q_table, data_manager, dict_tablecol, path_roicheck, local_var=True):
+
+
     options = QFileDialog.Options()
     path_Fall = self.dict_lineedit[f"path_fall_{key}"].text()
     dir_project = os.path.dirname(path_Fall)
@@ -47,49 +82,15 @@ def saveROICheck(data_manager):
     path_roicheck, _ = QFileDialog.getSaveFileName(self, "Save ROI Check", path_roicheck_init, "mat Files (*.mat);;All Files (*)", options=options)
     if path_roicheck:
         try:
-            tableWidget = self.dict_table[key]
-            today = datetime.datetime.today().strftime('%y%m%d')
-
-            cell_type_keys = ROICheckMatKeysLocal.cell_type_keys
-
-            data_to_save = {}
-            roi_count = tableWidget.rowCount()
-
-            for col_name, col_info in self.dict_tablecol.items():
-                if col_info['type'] == 'radio':
-                    selected_rows = []
-                    for row in range(roi_count):
-                        radio_button = tableWidget.cellWidget(row, col_info['order'])
-                        if radio_button and radio_button.isChecked():
-                            selected_rows.append([row])
-
-                    data_to_save[col_name] = np.array(selected_rows, dtype=np.int32)
-                    if col_name in cell_type_keys:
-                        data_to_save[cell_type_keys[col_name]] = np.array(selected_rows, dtype=np.int32)
-                elif col_info['type'] == 'checkbox':
-                    data_to_save[col_name] = np.zeros((roi_count, 1), dtype=np.bool_)
-                    for row in range(roi_count):
-                        item = tableWidget.item(row, col_info['order'])
-                        data_to_save[col_name][row] = item.checkState() == Qt.Checked if item else False
-                elif col_info['type'] == 'string':
-                    data_to_save[col_name] = np.empty((roi_count, 1), dtype=object)
-                    for row in range(roi_count):
-                        item = tableWidget.item(row, col_info['order'])
-                        data_to_save[col_name][row] = item.text() if item else ''
-
-            threshold_roi = {param: self.dict_lineedit[f"threshold_{param}"].text() for param in ["npix", "radius", "aspect_ratio", "compact", "skew", "std"]}
-            
-            mat_roicheck = {
-                "manualROIcheck": {
-                    **data_to_save,
-                    "update": today,
-                    "threshold_roi": threshold_roi,
-                }
-            }
+            dict_roicheck = convertTableDataToDictROICheck(q_table, dict_tablecol, local_var)
+            mat_roicheck = convertDictROICheckToMatROICheck(dict_roicheck)
             savemat(path_roicheck, mat_roicheck)
-            print("ROICheck file saved!")
+            QMessageBox.information(q_window, "File save", "ROICheck file saved!")
         except Exception as e:
-            print(f"Error saving ROICheck file: {e}")
+            QMessageBox.warning(q_window, "File save failed", f"Error saving ROICheck file: {e}")
+
+
+
 # ROICheckを読み込んでTableを更新
 def loadROICheck(self, key):
     options = QFileDialog.Options()
