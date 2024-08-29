8d4c9cc8379c02c9d12bf6880add62a16aa604f6
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2024-08-29 17:02:58 +0900

    update table_controls, moveToSameCellType

--- optic/controls/table_controls.py
+++ optic/controls/table_controls.py
@@ -21,8 +21,8 @@ class TableControls:
         if self.selected_row is not None and event.key() in self.key_function_map:
             action = self.key_function_map[event.key()]
             self.executeAction(action)
-            self.q_table.setCurrentCell(self.selected_row, 0)
-            self.q_table.scrollToItem(self.q_table.item(self.selected_row, 0))
+            self.q_table.setCurrentCell(self.selected_row, self.selected_column)
+            self.q_table.scrollToItem(self.q_table.item(self.selected_row, self.selected_column))
 
     def onSelectionChanged(self, selected, deselected):
         if selected.indexes():
@@ -35,6 +35,12 @@ class TableControls:
             self.selectRadioButton(self.selected_row, action[1]) # row, column
         elif action_type == 'checkbox':
             self.toggleCheckBox(self.selected_row, action[1]) # row, column
+        elif action_type == 'move':
+            move_type = action[1] # cell_type, skip_checked, skip_unchecked, selected_type
+            direction = action[2] # 1, -1
+            if move_type == 'cell_type':
+                self.moveToSameCellType(self.selected_row, direction)
+
 
     # select radiobutton
     def selectRadioButton(self, row, column):
@@ -53,3 +59,32 @@ class TableControls:
                 if check_box_item:
                     check_box_item.setCheckState(Qt.Unchecked if check_box_item.checkState() == Qt.Checked else Qt.Checked)
                 break
+
+    def getCurrentCellType(self, row):
+        for col_name, col_info in self.dict_tablecol.items():
+            if col_info['type'] == 'radio':
+                radio_button = self.q_table.cellWidget(row, col_info['order'])
+                if radio_button and radio_button.isChecked():
+                    return col_name
+        return None
+
+    def isRowChecked(self, row):
+        for col_name, col_info in self.dict_tablecol.items():
+            if col_info['type'] == 'checkbox' and col_name == 'Check':
+                check_box_item = self.q_table.item(row, col_info['order'])
+                return check_box_item.checkState() == Qt.Checked if check_box_item else False
+        return False
+
+    # move Neuron->Neuron, Astrocyte->Astrocyte
+    def moveToSameCellType(self, start_row, direction):
+        current_cell_type = self.getCurrentCellType(start_row)
+        total_rows = self.q_table.rowCount()
+        new_row = start_row
+
+        while True:
+            new_row = (new_row + direction) % total_rows
+            if new_row == start_row:
+                break
+            if self.getCurrentCellType(new_row) == current_cell_type:
+                self.selected_row = new_row
+                return
