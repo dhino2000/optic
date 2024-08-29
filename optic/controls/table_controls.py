1f44d5e7f4f7ea4594c7e79179af1a48f9b2678a
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2024-08-29 16:53:28 +0900

    update table_controls, selectRadioButton, toggleCheckBox 上下キーで列方向移動ができないバグあり

--- optic/controls/table_controls.py
+++ optic/controls/table_controls.py
@@ -17,9 +17,39 @@ class TableControls:
         # 選択を変更したときの関数
         self.q_table.selectionModel().selectionChanged.connect(self.onSelectionChanged)
 
+    def keyPressEvent(self, event):
+        if self.selected_row is not None and event.key() in self.key_function_map:
+            action = self.key_function_map[event.key()]
+            self.executeAction(action)
+            self.q_table.setCurrentCell(self.selected_row, 0)
+            self.q_table.scrollToItem(self.q_table.item(self.selected_row, 0))
 
     def onSelectionChanged(self, selected, deselected):
         if selected.indexes():
             self.selected_row = selected.indexes()[0].row()
             self.selected_column = selected.indexes()[0].column()
+
+    def executeAction(self, action):
+        action_type = action[0] # radio, checkbox, move
+        if action_type == 'radio':
+            self.selectRadioButton(self.selected_row, action[1]) # row, column
+        elif action_type == 'checkbox':
+            self.toggleCheckBox(self.selected_row, action[1]) # row, column
+
+    # select radiobutton
+    def selectRadioButton(self, row, column):
+        for col_name, col_info in self.dict_tablecol.items():
+            if col_info['type'] == 'radio' and col_info['order'] == column:
+                radio_button = self.q_table.cellWidget(row, column)
+                if isinstance(radio_button, QRadioButton):
+                    radio_button.setChecked(True)
+                break
     
+    # check/uncheck checkbox
+    def toggleCheckBox(self, row, column):
+        for col_name, col_info in self.dict_tablecol.items():
+            if col_info['type'] == 'checkbox' and col_info['order'] == column:
+                check_box_item = self.q_table.item(row, column)
+                if check_box_item:
+                    check_box_item.setCheckState(Qt.Unchecked if check_box_item.checkState() == Qt.Checked else Qt.Checked)
+                break
