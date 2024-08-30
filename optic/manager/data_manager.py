d6f329877bbcab6ea2ec449ad98fa18ea7ee37d4
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2024-08-30 15:47:43 +0900

    update data_manager, dict_im_bg, dict_im_chan2

--- optic/manager/data_manager.py
+++ optic/manager/data_manager.py
@@ -1,9 +1,10 @@
 class DataManager:
     def __init__(self):
-        self.dict_Fall      = {}
-        self.dict_im        = {}
-        self.dict_eventfile = {}
-        self.dict_roicheck  = {}
+        self.dict_Fall         = {}
+        self.dict_im_bg        = {}
+        self.dict_im_chan2     = {}
+        self.dict_eventfile    = {}
+        self.dict_roicheck     = {}
         self.dict_selected_roi = {}
 
     def setSelectedROI(self, key, roi_id):
