968c4ae2704ff0fed0eeb3d1ceaf556ef85f32db
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2024-09-05 16:05:14 +0900

    Refactor ControlManager to include shared attributes functionality
    Update the ControlManager class to include shared attributes functionality. This involves adding a dictionary called `shared_attr` to hold attributes shared between controls. The dictionary has two levels of keys: `app_key` and `attr_key`. Additionally, two new methods `setSharedAttr` and `getSharedAttr` are added to set and retrieve shared attributes, respectively. The `getAllSharedAttrs` method is also added to retrieve all shared attributes for a given `app_key`.
    
    These changes improve the functionality and maintainability of the code.

--- optic/manager/control_manager.py
+++ optic/manager/control_manager.py
@@ -1,5 +1,22 @@
+from collections import defaultdict
+
 # Manager controls class
 class ControlManager:
     def __init__(self):
         self.table_controls = {}
         self.view_controls = {}
+        """
+        Dictionary to hold attributes shared between controls
+        The first level key is `app_key`: "pri", "sec", etc.
+        The second level key is `attr_key`: "selected_roi", etc.
+        """
+        self.shared_attr = defaultdict(dict)
+
+    def setSharedAttr(self, app_key: str, attr_key: str, value):
+        self.shared_attr[app_key][attr_key] = value
+
+    def getSharedAttr(self, app_key: str, attr_key: str):
+        return self.shared_attr[app_key].get(attr_key)
+
+    def getAllSharedAttrs(self, app_key: str):
+        return self.shared_attr[app_key]
