d9b29308b9130386b312e8aed836cffbc9633aeb
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2024-09-11 12:37:31 +0900

    Add new ROI info layouts to GUI initialization

--- optic/gui/__init__.py
+++ optic/gui/__init__.py
@@ -2,6 +2,7 @@ from ..manager.widget_manager import WidgetManager
 from .app_setup import setupMainWindow, clearLayout
 from .app_style import setAppStyle, applyAppStyle
 from .base_layouts import makeLayoutLineEditLabel, makeLayoutButtonGroup, makeLayoutSliderLabel
+from .info_layouts import makeLayoutROIProperty, makeLayoutROICount
 from .io_layouts import makeLayoutLoadFileWidget, makeLayoutROICheckIO, makeLayoutLoadFileExitHelp
 from .plot_layouts import makeLayoutCanvasTracePlot
 from .slider_layouts import makeLayoutContrastSlider, makeLayoutOpacitySlider
