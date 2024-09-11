9af31dd9b4c3ca5e6f98b85c615cc62e0d60ae86
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2024-09-11 10:57:01 +0900

    Refactor imports and update default app style in Suite2pROICheck_ver02.ipynb and optic/gui/__init__.py

--- optic/gui/app_style.py
+++ optic/gui/app_style.py
@@ -1,16 +1,22 @@
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QDesktopWidget
 from PyQt5.QtGui import QFont
 
-def setAppStyle(app: QApplication, font_name: str = "MS UI Gothic", font_size: int = 12, widget_height: int = 20, widget_margin: int = 2):
+def setAppStyle(app: QApplication, font_name: str = "MS UI Gothic", base_font_size: int = 12, base_widget_height: int = 20, base_widget_margin: int = 2):
     """
     アプリケーション全体のスタイルを設定する。
 
     :param app: QApplicationインスタンス
     :param font_name: フォント名（デフォルトは "MS UI Gothic"）
-    :param font_size: フォントサイズ
-    :param widget_height: ウィジェットの高さ
-    :param widget_margin: ウィジェットの余白
+    :param base_font_size: 基本のフォントサイズ
+    :param base_widget_height: 基本のウィジェットの高さ
+    :param base_widget_margin: 基本のウィジェットの余白
     """
+    scaling_factor = getScalingFactor()
+    
+    font_size = int(base_font_size * scaling_factor)
+    widget_height = int(base_widget_height * scaling_factor)
+    widget_margin = int(base_widget_margin * scaling_factor)
+
     # フォントの設定
     font = QFont(font_name, font_size)
     app.setFont(font)
@@ -41,3 +47,21 @@ def applyAppStyle(app: QApplication):
     アプリケーションにデフォルトのスタイルを適用する。
     """
     setAppStyle(app)
+
+def getScalingFactor():
+    """
+    ディスプレイの横幅に基づいてスケーリングファクターを計算する。
+    """
+    screen = QDesktopWidget().screenNumber(QDesktopWidget().cursor().pos())
+    width = QDesktopWidget().screenGeometry(screen).width()
+    
+    if width < 2400:
+        return 1
+    elif width < 2880:
+        return 1.25
+    elif width < 3360:
+        return 1.5
+    elif width < 3840:
+        return 1.75
+    else:
+        return 2
