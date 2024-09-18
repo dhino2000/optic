3012344d473ca9e5de759de617fc1a9ce4f1e190
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2024-09-18 11:04:59 +0900

    Refactor table control to update checkbox key in Suite2pROICheck_ver02.ipynb and optic/gui

---
+++ optic/visualization/plot_visual.py
@@ -0,0 +1,12 @@
+from typing import Dict, Any
+import numpy as np
+
+def plotTraces(ax, traces: Dict[str, np.ndarray], colors: Dict[str, str], labels: Dict[str, str], title: str):
+    ax.clear()
+    for key in traces.items():
+        ax.plot(traces[key], color=colors[key], label=labels[key])
+
+    if title:
+        ax.set_title(title)
+
+    ax.legend()
