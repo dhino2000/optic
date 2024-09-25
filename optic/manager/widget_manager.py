5b76c80c63bf669fd21638c44f0be882ef5629df
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2024-09-25 17:50:44 +0900

    Refactor function names and improve widget management

--- optic/manager/widget_manager.py
+++ optic/manager/widget_manager.py
@@ -1,24 +1,31 @@
+from __future__ import annotations
+from ..type_definitions import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.axes import Axes
 
 # Widget作成関数
 """
 make Widget Function
 """
 # QLabel Widget
-def makeWidgetLabel(label, 
-                    align=Qt.AlignLeft):
+def makeWidgetLabel(
+    label: str, 
+    align: Qt.AlignmentFlag=Qt.AlignLeft
+) -> QLabel:
     widget = QLabel(label)
     widget.setAlignment(align)
     return widget
 
 # QCheckBox Widget
-def makeWidgetCheckBox(label, 
-                       func_=None, 
-                       checked=False):
+def makeWidgetCheckBox(
+    label: str, 
+    func_: Callable[..., Any]=None, 
+    checked: bool=False
+) -> QCheckBox:
     widget = QCheckBox(label)
     widget.setChecked(checked) 
     if func_:
@@ -26,8 +33,10 @@ def makeWidgetCheckBox(label,
     return widget
 
 # QLineEdit
-def makeWidgetLineEdit(text_set="", 
-                       width_fix=0):
+def makeWidgetLineEdit(
+    text_set: str="", 
+    width_fix: int=0
+) -> QLineEdit:
     widget = QLineEdit()
     if width_fix:
         widget.setFixedWidth(width_fix)
@@ -35,12 +44,14 @@ def makeWidgetLineEdit(text_set="",
     return widget
 
 # QSlider Widget
-def makeWidgetSlider(func_=None, 
-                     value_min=0, 
-                     value_max=255, 
-                     value_set=10, 
-                     height=10, 
-                     axis=Qt.Horizontal):
+def makeWidgetSlider(
+    func_: Callable[..., Any]=None, 
+    value_min: int=0, 
+    value_max:int =255, 
+    value_set:int =10, 
+    height:int =10, 
+    axis:Qt.Orientation =Qt.Horizontal
+) -> QSlider:
     widget = QSlider(axis)
     widget.setMinimum(value_min)
     widget.setMaximum(value_max)
@@ -51,24 +62,28 @@ def makeWidgetSlider(func_=None,
     return widget
 
 # QPushButton Widget
-def makeWidgetButton(label, 
-                     func_=None):
+def makeWidgetButton(
+    label: str, 
+    func_: Callable[..., Any]=None
+) -> QPushButton:
     widget = QPushButton(label)
     if func_:
         widget.clicked.connect(func_)
     return widget
 
 # QTable Widget
-def makeWidgetTable():
+def makeWidgetTable() -> QTableWidget:
     widget = QTableWidget()
     return widget
 
 # QTable ListWidget
-def makeWidgetListWidget(parent=None, 
-                      items=None, 
-                      selection_mode=QListWidget.SingleSelection,
-                      editable=False,
-                      drag_drop_mode=QListWidget.NoDragDrop):
+def makeWidgetListWidget(
+    parent: Any=None, 
+    items: List[Any]=None, 
+    selection_mode: QAbstractItemView.SelectionMode=QListWidget.SingleSelection, 
+    editable: bool=False, 
+    drag_drop_mode: QAbstractItemView.DragDropMode=QListWidget.NoDragDrop
+) -> QListWidget:
     """
     QListWidgetを作成し、設定する関数
 
@@ -96,34 +111,40 @@ def makeWidgetListWidget(parent=None,
     return widget
 
 # ComboBox Widget
-def makeWidgetComboBox(items=None):
+def makeWidgetComboBox(
+    items: List[Any]=None
+) -> QComboBox:
     widget = QComboBox()
     if items:
         widget.addItems(items)
     return widget
 
 # Figure Widget
-def makeWidgetFigure():
+def makeWidgetFigure() -> Figure:
     widget = Figure()
     return widget
 
 # FiugreCanvasQTAgg Widget
-def makeWidgetFigureCanvas(figure):
+def makeWidgetFigureCanvas(
+    figure: Figure
+) -> FigureCanvasQTAgg:
     widget = FigureCanvasQTAgg(figure)
     return widget
 
 # QGraphicsScene Widget
-def makeWidgetScene():
+def makeWidgetScene() -> QGraphicsScene:
     widget = QGraphicsScene()
     return widget
 
 # QGraphicsView Widget
-def makeWidgetView(scene, 
-                   width_min=0, 
-                   height_min=0, 
-                   color="black", 
-                   anti_aliasing=True, 
-                   smooth_pixmap_transform=True):
+def makeWidgetView(
+    scene: QGraphicsScene, 
+    width_min: int=0, 
+    height_min: int=0, 
+    color: str="black", 
+    anti_aliasing: bool=True, 
+    smooth_pixmap_transform: bool=True
+) -> QGraphicsView:
     widget = QGraphicsView(scene)
     if width_min:
         widget.setMinimumHeight(width_min)
@@ -138,10 +159,12 @@ def makeWidgetView(scene,
     return widget
 
 # figureのaxisに追加, 位置を指定
-def addWidgetAxisOnFigure(figure, 
-                          num_row, 
-                          num_col, 
-                          position):
+def addWidgetAxisOnFigure(
+    figure: Figure, 
+    num_row: int, 
+    num_col: int, 
+    position: int
+) -> Figure:
     figure = figure.add_subplot(num_row, num_col, position)
     return figure
 
@@ -152,70 +175,140 @@ GUI Widget管理用クラス
 """
 class WidgetManager:
     def __init__(self):
-        self.dict_label       = {}
-        self.dict_lineedit    = {}
-        self.dict_button      = {} 
-        self.dict_ax          = {} 
-        self.dict_checkbox    = {} 
-        self.dict_slider      = {}
-        self.dict_combobox    = {} 
-        self.dict_listwidget  = {} 
-        self.dict_buttongroup = {}
-        self.dict_scene       = {}
-        self.dict_view        = {}
-        self.dict_table       = {}
-        self.dict_figure      = {}
-        self.dict_canvas      = {}
+        self.dict_label       :Dict[str, QLabel] = {}
+        self.dict_lineedit    :Dict[str, QLineEdit] = {}
+        self.dict_button      :Dict[str, QPushButton] = {} 
+        self.dict_ax          :Dict[str, Axes] = {} 
+        self.dict_checkbox    :Dict[str, QCheckBox] = {} 
+        self.dict_slider      :Dict[str, QSlider] = {}
+        self.dict_combobox    :Dict[str, QComboBox] = {} 
+        self.dict_listwidget  :Dict[str, QListWidget] = {} 
+        self.dict_buttongroup :Dict[str, QButtonGroup] = {}
+        self.dict_scene       :Dict[str, QGraphicsScene] = {}
+        self.dict_view        :Dict[str, QGraphicsView] = {}
+        self.dict_table       :Dict[str, QTableWidget] = {}
+        self.dict_figure      :Dict[str, Figure] = {}
+        self.dict_canvas      :Dict[str, FigureCanvasQTAgg] = {}
 
-    def makeWidgetLabel(self, key, label, align=Qt.AlignLeft):
+    def makeWidgetLabel(
+        self, 
+        key: str, 
+        label: str, 
+        align: Qt.AlignmentFlag = Qt.AlignLeft
+    ) -> QLabel:
         self.dict_label[key] = makeWidgetLabel(label, align)
         return self.dict_label[key]
     
-    def makeWidgetCheckBox(self, key, label, func_=None, checked=False):
+    def makeWidgetCheckBox(
+        self, 
+        key: str, 
+        label: str, 
+        func_: Callable[..., Any] = None, 
+        checked: bool = False
+    ) -> QCheckBox:
         self.dict_checkbox[key] = makeWidgetCheckBox(label, func_, checked)
         return self.dict_checkbox[key]
     
-    def makeWidgetLineEdit(self, key, text_set="", width_fix=None):
+    def makeWidgetLineEdit(
+        self, 
+        key: str, 
+        text_set: str = "", 
+        width_fix: int = 0
+    ) -> QLineEdit:
         self.dict_lineedit[key] = makeWidgetLineEdit(text_set, width_fix)
         return self.dict_lineedit[key]
     
-    def makeWidgetSlider(self, key, func_=None, value_min=0, value_max=255, value_set=10, height=10, axis=Qt.Horizontal):
+    def makeWidgetSlider(
+        self, 
+        key: str, 
+        func_: Callable[..., Any] = None, 
+        value_min: int = 0, 
+        value_max: int = 255, 
+        value_set: int = 10, 
+        height: int = 10, 
+        axis: Qt.Orientation = Qt.Horizontal
+    ) -> QSlider:
         self.dict_slider[key] = makeWidgetSlider(func_, value_min, value_max, value_set, height, axis)
         return self.dict_slider[key]
     
-    def makeWidgetButton(self, key, label, func_=None):
+    def makeWidgetButton(
+        self, 
+        key: str, 
+        label: str, 
+        func_: Callable[..., Any] = None
+    ) -> QPushButton:
         self.dict_button[key] = makeWidgetButton(label, func_)
         return self.dict_button[key]
 
-    def makeWidgetTable(self, key):
+    def makeWidgetTable(
+        self, 
+        key: str
+    ) -> QTableWidget:
         self.dict_table[key] = makeWidgetTable()
         return self.dict_table[key]
     
-    def makeWidgetListWidget(self, key, parent=None, items=None, selection_mode=QListWidget.SingleSelection, editable=False, drag_drop_mode=QListWidget.NoDragDrop):
+    def makeWidgetListWidget(
+        self, 
+        key: str, 
+        parent: Any=None, 
+        items: List[Any]=None, 
+        selection_mode: QAbstractItemView.SelectionMode=QListWidget.SingleSelection, 
+        editable: bool=False, 
+        drag_drop_mode: QAbstractItemView.DragDropMode=QListWidget.NoDragDrop
+    ) -> QListWidget:
         self.dict_listwidget[key] = makeWidgetListWidget(parent, items, selection_mode, editable, drag_drop_mode)
         return self.dict_listwidget[key]
     
-    def makeWidgetComboBox(self, key, items=None):
+    def makeWidgetComboBox(
+        self, 
+        key: str, 
+        items: List[Any]=None
+    ) -> QComboBox:
         self.dict_combobox[key] = makeWidgetComboBox(items)
         return self.dict_combobox[key]
 
-    def makeWidgetFigure(self, key):
+    def makeWidgetFigure(
+        self, 
+        key: str
+    ) -> Figure:
         self.dict_figure[key] = Figure()
         return self.dict_figure[key]
 
-    def makeWidgetFigureCanvas(self, key, figure):
+    def makeWidgetFigureCanvas(
+        self, 
+        key: str, 
+        figure: Figure
+    ) -> FigureCanvasQTAgg:
         self.dict_canvas[key] = FigureCanvasQTAgg(figure)
         return self.dict_canvas[key]
 
-    def makeWidgetScene(self, key):
+    def makeWidgetScene(
+        self, 
+        key: str
+    ) -> QGraphicsScene:
         self.dict_scene[key] = makeWidgetScene()
         return self.dict_scene[key]
 
-    def makeWidgetView(self, key, width_min=None, height_min=None, color="black", anti_aliasing=True, smooth_pixmap_transform=True):
+    def makeWidgetView(
+        self, 
+        key: str, 
+        width_min: int=0, 
+        height_min: int=0, 
+        color: str="black", 
+        anti_aliasing: bool=True, 
+        smooth_pixmap_transform: bool=True
+    ) -> QGraphicsView:
         scene = self.makeWidgetScene(key)
         self.dict_view[key] = makeWidgetView(scene, width_min, height_min, color, anti_aliasing, smooth_pixmap_transform)
         return self.dict_view[key]
 
-    def addWidgetAxisOnFigure(self, key, figure, num_row, num_col, position):
+    def addWidgetAxisOnFigure(
+        self, 
+        key: str, 
+        figure: Figure, 
+        num_row: int, 
+        num_col: int, 
+        position: int
+    ) -> Axes:
         self.dict_ax[key] = figure.add_subplot(num_row, num_col, position)
         return self.dict_ax[key]
