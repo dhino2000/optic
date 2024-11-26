b7e32e24f4057a5d56635d2b0ca80737e3a985cd
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2024-11-26 09:49:31 +0900

    add suite2p

--- external/__init__.py
+++ external/__init__.py
@@ -5,5 +5,9 @@ EXTERNAL_DIR = Path(__file__).parent.resolve()
 
 # Add external package directory to sys.path
 FGW_PATH = EXTERNAL_DIR / "FGW-master" / "lib"
+SUITE2P_PATH = EXTERNAL_DIR / "suite2p"
+
 if str(FGW_PATH) not in sys.path:
     sys.path.append(str(FGW_PATH))
+if str(SUITE2P_PATH) not in sys.path:
+    sys.path.append(str(SUITE2P_PATH))
