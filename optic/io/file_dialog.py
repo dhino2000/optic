d6aae2aa12a4b913327102baff2a676157e8bf52
dhino <fukatsu.noriaki.i2@s.mail.nagoya-u.ac.jp>
2026-01-10 16:59:35 +0900

    update: fix bug of saveFileDialog, update CheckMultiSessionROICoordinates; export view image, draw heatmap of ROI unmerging area

--- optic/io/file_dialog.py
+++ optic/io/file_dialog.py
@@ -52,7 +52,15 @@ def saveFileDialog(
         ) -> Tuple[Optional[str], bool]:
     from ..config.constants import FILE_FILTERS
     options = QFileDialog.Options()
-    file_filter = FILE_FILTERS.get(file_type, "All Files (*)")
+
+    # Handle single or multiple file types
+    if isinstance(file_type, list):
+        # Combine multiple file filters
+        filter_list = [FILE_FILTERS.get(ft, f"*{ft}") for ft in file_type]
+        file_filter = ";;".join(filter_list) + ";;All Files (*)"
+    else:
+        file_filter = FILE_FILTERS.get(file_type, "All Files (*)")
+        
     while True:
         file_path, _ = QFileDialog.getSaveFileName(q_widget, title, initial_dir, file_filter, options=options)
         if not file_path:
